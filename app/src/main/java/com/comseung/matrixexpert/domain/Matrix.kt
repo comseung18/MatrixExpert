5cf5974cce86b61540a245771908b235500b8385
comseung18 <40685565+comseung18@users.noreply.github.com>
2023-03-12 00:14:44 +0900

    Update Matrix.kt
    행교환 연산 추가, toString 재정의, 인덱서 추가

--- app/src/main/java/com/comseung/matrixexpert/domain/Matrix.kt
+++ app/src/main/java/com/comseung/matrixexpert/domain/Matrix.kt
@@ -1,16 +1,56 @@
 package com.comseung.matrixexpert.domain
 
+import kotlin.math.round
+
 class Matrix(
     val rows : Int = 1,
     val cols : Int = 1
 ) {
 
-    val data : Array<DoubleArray>
+    private val data : Array<DoubleArray>
     init {
         if(rows <= 0 ||
             cols <= 0) throw IllegalArgumentException()
 
         data = Array(rows) { DoubleArray(cols) }
+    }
+
+    operator fun get(r: Int) = data[r]
+    operator fun get(r: Int, c: Int) = data[r][c]
+    operator fun set(r: Int, value: DoubleArray) {
+        data[r] = value
+    }
+    operator fun set(r: Int, c: Int, value: Double) {
+        data[r][c] = value
+    }
+
+    fun rowExchange(r1 : Int, r2 : Int ): Unit {
+        if(r1 !in 0 until rows ||
+                r2 !in 0 until rows) throw IllegalArgumentException()
+
+        if(r1 == r2) return
+
+        val swap = data[r1].copyOf()
+        data[r1] = data[r2].copyOf()
+        data[r2] = swap
+    }
+
+
+
+    override fun toString(): String {
+        val buffer = StringBuilder()
+        buffer.append("==========\n")
+        data.forEach { row ->
+            row.joinTo(
+                buffer,
+                separator = " | ",
+                postfix = "\n",
+            ) {
+                (round(it*10)/10).toString()
+            }
+        }
+        buffer.append("==========\n")
+        return buffer.toString()
     }
 }
 
@@ -20,10 +60,17 @@ fun zeros(n : Int) = zeros(n, n)
 fun ones(rows: Int, cols: Int) : Matrix {
     val ret = Matrix(rows, cols)
     for(r in 0 until rows)
-        for(c in 0 until cols)
-            ret.data[r][c] = 1.0
+            ret[r] = DoubleArray(cols) { 1.0 }
     return ret
 }
 fun ones(n: Int) = ones(n, n)
 
+fun identify(rows: Int, cols: Int) : Matrix {
+    val ret = Matrix(rows, cols)
+    for(i in 0 until rows)
+        ret[i][i] = 1.0
+
+    return ret
+}
+fun identify(n: Int) = identify(n, n)
 
